70386fb139f0798c0b9d9a918dd959d3791793ea
Lindsay Evans <linz@linz.id.au>
2012-04-23 22:14:55 +1000

    Adding comments

--- outline.js
+++ outline.js
@@ -1,8 +1,11 @@
+// outline.js
+// based on http://www.paciellogroup.com/blog/2012/04/how-to-remove-css-outlines-in-an-accessible-manner/
 (function(d){
 
 	var style_element = d.createElement('STYLE');
 	d.getElementsByTagName('HEAD')[0].appendChild(style_element);
 
+	// Using mousedown instead of mouseover, so that previously focused elements don't lose focus ring on mouse move
 	d.onmousedown = function(){
 		style_element.innerHTML = 'a{outline:none}';
 	};
