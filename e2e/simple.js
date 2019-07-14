9207c4edd651314071beff5889fca34572e227d5
Lindsay Evans <lindsay.evans@boomworks.com.au>
2019-07-14 14:47:17 +1000

    Removing Nightwatch/BrowserStack

--- e2e/simple.js
+++
@@ -1,9 +0,0 @@
-export default {
-    'Local server': browser => {
-        browser
-            .url('http://localhost:8080/')
-            .waitForElementVisible('body', 1000)
-            .assert.title('outline.js')
-            .end()
-    }
-}
