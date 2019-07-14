a5239c79c0c207b283a297dd07f0c8a511c9634f
Lindsay Evans <lindsay.evans@boomworks.com.au>
2019-07-14 15:54:48 +1000

    Adding e2e tests for show/hide focus

--- cypress/integration/simple.ts
+++ cypress/integration/simple.ts
@@ -2,7 +2,28 @@ describe('Simple page', () => {
     it('loads', () => {
         cy.visit('/examples/');
         cy.get('h1');
-        cy.percySnapshot('Simple page loads', {
+    });
+
+    it('displays focus rectangle on tab navigation', () => {
+        cy.visit('/examples/');
+        cy.get('.test-link').focus();
+        cy.percySnapshot('Simple page - link shows focus', {
+            widths: [992]
+        });
+        cy.get('.test-button').focus();
+        cy.percySnapshot('Simple page - button shows focus', {
+            widths: [992]
+        });
+    });
+
+    it('doesnt display focus rectangle on mouse navigation', () => {
+        cy.visit('/examples/');
+        cy.get('.test-link').click();
+        cy.percySnapshot('Simple page - link hides focus', {
+            widths: [992]
+        });
+        cy.get('.test-button').click();
+        cy.percySnapshot('Simple page - button hides focus', {
             widths: [992]
         });
     });
